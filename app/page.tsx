d56bf84c2609bc48bfed533de67fa890bebb2151
t-t-bui <tbui931958@gmail.com>
2025-09-24 22:20:59 -0500

    adding nav

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import Image from "next/image";
+import Nav from "./nav";
+
+import { Analytics } from '@vercel/analytics/next';
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+      <Nav/>
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <h2>Dashboard</h2>
         <Image
