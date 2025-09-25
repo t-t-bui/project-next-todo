65fd9c7d84e27e3ddfe71673e223183bb288577f
t-t-bui <tbui931958@gmail.com>
2025-09-25 09:13:25 -0500

    fix nav

--- app/nav.tsx
+++ app/nav.tsx
@@ -1,8 +1,31 @@
 export default function Nav() {
 	return (
-		<div className="">
-			<h2>Nav</h2>
-		</div>
+		<nav className="relative flex bg-gray-800/50 after:pointer-events-none after:absolute after:inset-x-0 after:bottom-0 after:h-px after:bg-white/10">
+			<div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+				<div className="relative flex h-16 items-center justify-between">
+					<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+						{/* Mobile menu button */}
+						<button type="button" command="--toggle" commandfor="mobile-menu" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-white/5 hover:text-white focus:outline-2 focus:-outline-offset-1 focus:outline-indigo-500">
+							<span className="absolute -inset-0.5"></span>
+							<span className="sr-only">Open main menu</span> 
+						</button>
+					</div>
+					<div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
+						<div className="flex shrink-0 items-center">
+							<img src="https://tailwincss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500" alt="Your Company" className="h-8 w-auto" />
+						</div>
+						<div className="hidden sm:ml-6 sm:block">
+							<div className="flex space-x-4">
+								<a href="#" aria-current="page" className="rounded-md bg-gray-950/50 px-3 py-2 text-sm font-medium text-white">Dashboard</a>
+								<a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Team</a>
+								<a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Projecs</a>
+								<a href="#" className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Calendar</a>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</nav>
 	);
 }
 
